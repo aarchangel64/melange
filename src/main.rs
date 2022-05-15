ff74b0fe604c7450fbf05bd0641b1ca0d5ea085a
Shrey Pasricha <shrey.pasricha@gmail.com>
2022-05-15 09:21:01 -0400

    Stupid idea to resize window as it opens, doesn't really work without jank.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
+use keyframe::{ease, functions::EaseIn};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+use winit::dpi::{LogicalSize, PhysicalPosition};
 use winit::event_loop::EventLoop;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     rotation: f64,  // Rotation for the square.
     size: f64,
+    time: f64,
 }
 
 impl App {
@@ -48,7 +51,8 @@ impl App {
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
-        self.size += 50.0 * args.dt;
+        // self.size += 50.0 * args.dt;
+        self.time += args.dt;
     }
 }
 
@@ -56,13 +60,14 @@ fn main() {
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
+    // Use winit's EventLoop to get the available monitors
     let event = EventLoop::new();
     let mon = event.available_monitors().next().unwrap();
-    let mon_width = (mon.size().width as f64 / mon.scale_factor()) as u32 + 5;
-    let mon_height = (mon.size().height as f64 / mon.scale_factor()) as u32 + 5;
+    let mon_width = (mon.size().width as f64 / mon.scale_factor()) as i32 + 5;
+    let mon_height = (mon.size().height as f64 / mon.scale_factor()) as i32 + 5;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("Logout-HUD", [mon_width, mon_height])
+    let mut window: Window = WindowSettings::new("Logout-HUD", [10, 10])
         .graphics_api(opengl)
         .fullscreen(false)
         .resizable(false)
@@ -76,17 +81,35 @@ fn main() {
     let pos = gwindow.primary_monitor().unwrap().position();
     gwindow.set_always_on_top(true);
     gwindow.set_outer_position(pos);
+    gwindow.set_inner_size(LogicalSize::new(mon_width, mon_height));
 
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
-        size: 0.0,
+        size: 50.0,
+        time: 0.0,
     };
+
+    // let mut sequence = keyframes![
+    // (0.0, 0.0),
+    // (1.)]
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
+            let width = ease(EaseIn, 1.0, mon_width as f64, app.time / 5.0);
+            let height = ease(EaseIn, 1.0, mon_width as f64, app.time / 5.0);
+            let pos_x = pos.x + (mon_width - width as i32) / 2;
+            let pos_y = pos.y + (mon_height - height as i32) / 2;
+            window
+                .ctx
+                .window()
+                .set_outer_position(PhysicalPosition::new(pos_x, pos_y));
+            window
+                .ctx
+                .window()
+                .set_inner_size(LogicalSize::new(width, height));
             app.render(&args);
         }
 
