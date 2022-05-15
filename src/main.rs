7d84953c99487661df8d4608a82b1ce5d214a750
Shrey Pasricha <shrey.pasricha@gmail.com>
2022-05-15 10:20:26 -0400

    Made box that opens outward instead of resizing window.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
+// use glfw_window::GlfwWindow as Window;
+use keyframe::functions::Linear;
 use keyframe::{ease, functions::EaseIn};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 use winit::dpi::{LogicalSize, PhysicalPosition};
-use winit::event_loop::EventLoop;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     rotation: f64,  // Rotation for the square.
-    size: f64,
     time: f64,
 }
 
@@ -26,11 +26,19 @@ impl App {
         const BACKGROUND: [f32; 4] = [0.1, 0.1, 0.1, 0.6];
         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
+        let size = ease(Linear, 0.0, 1.0, (self.time / 1.0).clamp(0.0, 1.0));
+
         let (x, y) = (
-            (args.window_size[0] - self.size) / 2.0,
-            (args.window_size[1] - self.size) / 2.0,
+            args.window_size[0] * (1.0 - size) / 2.0,
+            args.window_size[1] * (1.0 - size) / 2.0,
         );
-        let square = rectangle::square(x, y, self.size);
+
+        let square = rectangle::rectangle_by_corners(
+            x,
+            y,
+            x + size * args.window_size[0],
+            y + size * args.window_size[1],
+        );
         let rotation = self.rotation;
 
         self.gl.draw(args.viewport(), |c, gl| {
@@ -49,10 +57,10 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
+        self.time += args.dt;
+
         // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
-        // self.size += 50.0 * args.dt;
-        self.time += args.dt;
     }
 }
 
@@ -60,17 +68,11 @@ fn main() {
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
-    // Use winit's EventLoop to get the available monitors
-    let event = EventLoop::new();
-    let mon = event.available_monitors().next().unwrap();
-    let mon_width = (mon.size().width as f64 / mon.scale_factor()) as i32 + 5;
-    let mon_height = (mon.size().height as f64 / mon.scale_factor()) as i32 + 5;
-
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("Logout-HUD", [10, 10])
+    let mut window: Window = WindowSettings::new("Logout-HUD", [100, 100])
         .graphics_api(opengl)
         .fullscreen(false)
-        .resizable(false)
+        .resizable(true)
         .decorated(false)
         .transparent(true)
         .exit_on_esc(true)
@@ -78,16 +80,22 @@ fn main() {
         .unwrap();
 
     let gwindow = window.ctx.window();
-    let pos = gwindow.primary_monitor().unwrap().position();
+    let monitor = gwindow.current_monitor().unwrap();
+    let monitor_width = (monitor.size().width as f64 / monitor.scale_factor()) as i32;
+    let monitor_height = (monitor.size().height as f64 / monitor.scale_factor()) as i32;
+    let mut pos = monitor.position();
+    println!("{}", pos.x);
+    // pos.x += 500;
+    // pos.y += 50;
     gwindow.set_always_on_top(true);
+    // gwindow.set_outer_position(PhysicalPosition::new(0, 0));
     gwindow.set_outer_position(pos);
-    gwindow.set_inner_size(LogicalSize::new(mon_width, mon_height));
+    gwindow.set_inner_size(LogicalSize::new(monitor_width, monitor_height));
 
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
-        size: 50.0,
         time: 0.0,
     };
 
@@ -98,18 +106,6 @@ fn main() {
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
-            let width = ease(EaseIn, 1.0, mon_width as f64, app.time / 5.0);
-            let height = ease(EaseIn, 1.0, mon_width as f64, app.time / 5.0);
-            let pos_x = pos.x + (mon_width - width as i32) / 2;
-            let pos_y = pos.y + (mon_height - height as i32) / 2;
-            window
-                .ctx
-                .window()
-                .set_outer_position(PhysicalPosition::new(pos_x, pos_y));
-            window
-                .ctx
-                .window()
-                .set_inner_size(LogicalSize::new(width, height));
             app.render(&args);
         }
 
