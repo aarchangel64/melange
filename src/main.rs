72b7c2ba21a54d2d0969db2162ac29fdfcdf64e9
Shrey Pasricha <shrey.pasricha@gmail.com>
2022-05-17 10:32:35 -0400

    Rectangle drawing animation.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::borrow::Borrow;
+
 use fontconfig::Fontconfig;
 use glutin_window::GlutinWindow;
+use graphics::{line, Context, DrawState, Graphics, Line};
+use keyframe::EasingFunction;
 use opengl_graphics::{GlGraphics, GlyphCache, OpenGL, TextureSettings};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 use winit::dpi::LogicalSize;
 
-use keyframe::functions::{EaseInOutCubic, EaseInQuint, Linear};
+use keyframe::functions::{
+    EaseInCubic, EaseInOutCubic, EaseInQuint, EaseOut, EaseOutCubic, Linear,
+};
 use keyframe::{ease, functions::EaseIn};
 
 use fps_counter::*;
@@ -21,47 +27,104 @@ pub struct App<'a> {
 }
 
 impl App<'_> {
+    #[inline]
+    fn anim<F: EasingFunction>(
+        function: impl Borrow<F>,
+        seconds: f64,
+        offset: f64,
+        time: f64,
+    ) -> f64 {
+        return ease(
+            function,
+            0.0,
+            1.0,
+            ((time - offset) / seconds).clamp(0.0, 1.0),
+        );
+    }
+
+    fn anim_rect<F: EasingFunction>(
+        &mut self,
+        colour: [f32; 4],
+        radius: f64,
+        (mut width, mut height): (f64, f64),
+        (x, y): (f64, f64),
+        func: impl Borrow<F>,
+        mut seconds: f64,
+        ctx: Context,
+    ) {
+        width /= 2.0;
+        height /= 2.0;
+        seconds /= 4.0;
+
+        let mut draw_line =
+            |points: [f64; 4]| line(colour, radius, points, ctx.transform, &mut self.gl);
+
+        // Bottom
+        draw_line([
+            x - width,
+            y + height,
+            x + width * (2.0 * App::anim(EaseOut, seconds, 0.0, self.time) - 1.0),
+            y + height,
+        ]);
+        // Left
+        draw_line([
+            x + width,
+            y + height,
+            x + width,
+            y - height * (2.0 * App::anim(EaseOut, seconds, seconds, self.time) - 1.0),
+        ]);
+        // Top
+        draw_line([
+            x + width,
+            y - height,
+            x - width * (2.0 * App::anim(EaseOut, seconds, 2.0 * seconds, self.time) - 1.0),
+            y - height,
+        ]);
+        // Right
+        draw_line([
+            x - width,
+            y - height,
+            x - width,
+            y + height * (2.0 * App::anim(EaseOut, seconds, 3.0 * seconds, self.time) - 1.0),
+        ]);
+    }
+
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
         const BACKGROUND: [f32; 4] = [0.1, 0.1, 0.1, 0.6];
         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
-        let size = ease(EaseInOutCubic, 0.0, 1.0, (self.time / 1.5).clamp(0.0, 1.0));
+        let ctx = self.gl.draw_begin(args.viewport());
+        // Clear the screen.
+        clear(BACKGROUND, &mut self.gl);
 
-        let (x, y) = (
-            args.window_size[0] * (1.0 - size) / 2.0,
-            args.window_size[1] * (1.0 - size) / 2.0,
+        let transform = ctx.transform;
+        // .trans(x, y)
+        // .rot_rad(rotation)
+        // .trans(-25.0, -25.0);
+
+        self.anim_rect(
+            WHITE,
+            1.0,
+            (500.0, 500.0),
+            (1920.0 / 2.0, 1080.0 / 2.0),
+            EaseOut,
+            3.0,
+            ctx,
         );
 
-        let square = rectangle::rectangle_by_corners(
-            x,
-            y,
-            x + size * args.window_size[0],
-            y + size * args.window_size[1],
-        );
-        let rotation = self.rotation;
+        Text::new_color(WHITE, 32)
+            .draw(
+                format!("fps: {}, tps: {}", self.fps, self.ups).as_str(),
+                &mut self.glyph,
+                &ctx.draw_state,
+                transform.trans(10.0, 30.0).zoom(0.5),
+                &mut self.gl,
+            )
+            .unwrap();
 
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BACKGROUND, gl);
-
-            let transform = c.transform;
-            // .trans(x, y)
-            // .rot_rad(rotation)
-            // .trans(-25.0, -25.0);
-
-            Rectangle::new_border(WHITE, 1.0).draw(square, &c.draw_state, transform, gl);
-            Text::new_color([1.0, 1.0, 1.0, 1.0], 32)
-                .draw(
-                    format!("fps: {}, tps: {}", self.fps, self.ups).as_str(),
-                    &mut self.glyph,
-                    &c.draw_state,
-                    transform.trans(10.0, 30.0).zoom(0.5),
-                    gl,
-                )
-                .unwrap();
-        });
+        self.gl.draw_end();
     }
 
     fn update(&mut self, args: &UpdateArgs) {
